3e813b42de66a6c44e07620f04283df51157bdc2
Samir Reddigari <samirreddigari@gmail.com>
2020-03-14 11:06:25 -0400

    chore: add react app scaffolding

---
+++ front-end/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+function App() {
+
+    return (
+        <Container className="App">
+            <h1>Visual Programming Workspace</h1>
+        </Container>
+    )
+}
+
+export default App;
