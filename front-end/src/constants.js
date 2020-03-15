0fc8c3593a799ee6c1b1ec964ea83a3e75825c64
Samir Reddigari <samirreddigari@gmail.com>
2020-03-15 15:08:32 -0400

    chore: remove accidentally copied file

--- front-end/src/constants.js
+++
@@ -1,8 +0,0 @@
-// constants.js holds any string literals or enums that
-// might be used across the application
-
-const DATA_ENDPOINT = "https://cove-coding-challenge-api.herokuapp.com/reservations";
-const GRAPH_HEIGHT = 30;
-
-
-export { DATA_ENDPOINT, GRAPH_HEIGHT };
