2644a40ed5cbd6f26ec3fdffbbe565441ddd940b
Matt Thomas <matthew_thomas@hms.harvard.edu>
2020-03-18 12:38:20 -0400

    fix: Update URL patterns to avoid namespace conflicts

--- vp/workflow/urls.py
+++ vp/workflow/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('open', views.open, name='open'),
-    path('save', views.save, name='save'),
+    path('open', views.open_workflow, name='open workflow'),
+    path('save', views.save_workflow, name='save'),
 ]
