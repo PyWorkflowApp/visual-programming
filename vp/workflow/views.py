1c6db2438c77aae7c0a3bebfdf91a8f79cdf3b13
Matt Thomas <matthew_thomas@hms.harvard.edu>
2020-03-18 12:41:15 -0400

    fix: change from YAML to JSON
    The built-in JSON methods now work with Django session data. Avoided this initially because I thought the JSON format didn't retain all graph info, but at first glance a simple graph appears all there.

--- vp/workflow/views.py
+++ vp/workflow/views.py
@@ -1,52 +1,64 @@
 from django.http import JsonResponse
 import networkx as nx
+import json
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-def open(request):
-
+def open_workflow(request):
     # Open filename specified
-    if request.GET.get('file'):
-        path = os.path.join(BASE_DIR, request.GET.get('file'))
-        DG = nx.read_yaml(path)
+    file = request.GET.get('file')
+
+    # File requested matches JSON extension
+    if file is not None and file[-5:] == '.json':
+        path = os.path.join(BASE_DIR, file)
+
+        with open(path, 'r') as json_file:
+            json_data = json.load(json_file)
+            DG = nx.readwrite.json_graph.node_link_graph(json_data)
 
     # Create a test graph with 3 nodes
     else:
         DG = nx.DiGraph()
-
         DG.add_nodes_from([1, 2, 3])
         DG.add_edges_from([(1, 2), (2, 3)])
 
     # Construct response
     data = {
-        # 'graph': DG,
+        'graph': nx.readwrite.node_link_data(DG),
         'nodes': DG.number_of_nodes(),
         'message': 'Saving graph to session.',
     }
 
     # Store in session
     # Not currently working; DiGraph is not JSON serializable
-    request.session['graph'] = data
+    request.session['graph'] = data['graph']
 
     return JsonResponse(data)
 
-def save(request):
 
+def save_workflow(request):
     # Send error message if no graph is in session
     if request.session.get('graph') is None:
         return JsonResponse({'message': 'No graph exists.'})
 
+    graph = request.session.get('graph')
 
-    # Create a test graph with 4 nodes
-    DG = nx.DiGraph()
+    with open('graph.json', 'w') as outfile:
+        json.dump(graph, outfile)
 
-    DG.add_nodes_from([1, 2, 3, 4])
-    DG.add_edges_from([(1, 2), (1, 4), (2, 3)])
+    # # Create a test graph with 4 nodes
+    # DG = nx.DiGraph()
+    #
+    # DG.add_nodes_from([1, 2, 3, 4])
+    # DG.add_edges_from([(1, 2), (1, 4), (2, 3)])
+    #
+    # # Write to YAML file
+    # nx.write_yaml(DG, 'test.yaml')
 
-    # Write to YAML file
-    nx.write_yaml(DG, 'test.yaml')
-
-    return JsonResponse({'message': 'Saved the graph to YAML file!'})
+    return JsonResponse({
+        'graph': graph,
+        'message': 'Saved the graph to JSON file!'
+    })
