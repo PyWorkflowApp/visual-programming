72f4caee3185fb36766dd9683fc28fdb8d4a6a90
Matt Thomas <matthew_thomas@hms.harvard.edu>
2020-03-20 12:38:29 -0400

    feat: add_node to graph route
    Currently runs as a GET request. Node info hard-coded as demo; can swap out later.

---
+++ vp/node/views.py
@@ -0,0 +1,36 @@
+from django.http import JsonResponse
+import networkx as nx
+import json
+
+from .models import Node
+from workflow.models import Workflow, WorkflowException
+
+workflow = Workflow()
+
+
+def node(request):
+    """ Add new Node to graph
+
+    """
+
+    # Load workflow from session
+    workflow.retrieve_from_session(request)
+
+    # Check if a graph is present
+    if workflow.graph is None:
+        return JsonResponse({
+            'message': 'A workflow has not been created yet.'
+        }, status=404)
+
+    # Create a new Node with POST info
+    # TODO: id is num + 1; rest of info is hard-coded
+    num_nodes = workflow.graph.number_of_nodes()
+    new_node = Node(node_id=(num_nodes + 1), node_type="blank", num_ports_in=1, num_ports_out=1)
+
+    # Add Node to graph and re-save workflow to session
+    workflow.add_node(new_node)
+    workflow.store_in_session(request)
+
+    return JsonResponse({
+        'message': 'Added new node to graph with ID #' + str(num_nodes + 1)
+    })
