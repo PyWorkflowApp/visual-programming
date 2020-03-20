046122aeca1ca41c3a2d644ea024eacbcfa35184
Samir Reddigari <samirreddigari@gmail.com>
2020-03-19 23:17:18 -0400

    feat(ui): style ports and nodes like KNIME

--- front-end/src/components/CustomNode/CustomNodeWidget.js
+++ front-end/src/components/CustomNode/CustomNodeWidget.js
@@ -1,19 +1,36 @@
 import * as React from 'react';
+import * as _ from 'lodash';
 import { PortWidget } from '@projectstorm/react-diagrams';
 import '../../styles/CustomNode.css';
 
 
 export class CustomNodeWidget extends React.Component {
+
     render() {
+        const engine = this.props.engine;
+        const ports = _.values(this.props.node.getPorts());
+        // group ports by type (in/out)
+        const sortedPorts = _.groupBy(ports, p => p.options.type);
+        // create PortWidget array for each type
+        const portWidgets = {};
+        for (let portType in sortedPorts) {
+            portWidgets[portType] = sortedPorts[portType].map(port =>
+                <PortWidget engine={engine} port={port}>
+                        <div className="triangle-port" />
+                </PortWidget>
+            );
+        }
         return (
-            <div className="custom-node" style={{ borderColor: this.props.node.color }}>
-                <PortWidget engine={this.props.engine} port={this.props.node.getPort('in')}>
-                    <div className="circle-port" />
-                </PortWidget>
-                <PortWidget engine={this.props.engine} port={this.props.node.getPort('out')}>
-                    <div className="circle-port" />
-                </PortWidget>
+            <div className="custom-node-wrapper">
                 <div className="custom-node-name">{this.props.node.options.name}</div>
+                <div className="custom-node" style={{ borderColor: this.props.node.color }}>
+                    <div className="port-col port-col-in">
+                        { portWidgets["in"] }
+                    </div>
+                    <div className="port-col port-col-out">
+                        { portWidgets["out"] }
+                    </div>
+                </div>
             </div>
         );
     }
