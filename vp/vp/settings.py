711309cbec63e5810d8a880168444236010481e1
Matt Thomas <matthew_thomas@hms.harvard.edu>
2020-03-21 11:34:53 -0400

    feat: Add custom middleware as placeholder

--- vp/vp/settings.py
+++ vp/vp/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Custom middleware
+    'workflow.middleware.WorkflowMiddleware',
 ]
 
 ROOT_URLCONF = 'vp.urls'
