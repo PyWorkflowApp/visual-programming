09e396bd43894fd8d5c73852b4d6e28d8c63386a
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-03-22 18:31:58 -0500

    Adds method getSVGPath to plot path

--- front-end/src/components/CustomLink/CustomLinkModel.js
+++ front-end/src/components/CustomLink/CustomLinkModel.js
@@ -1,3 +1,4 @@
+import { BezierCurve } from '@projectstorm/geometry';
 import { DefaultLinkModel } from '@projectstorm/react-diagrams';
 
 /**
@@ -10,4 +11,31 @@ export class CustomLinkModel extends DefaultLinkModel {
           width: 5
         });
       }
+
+    getSVGPath(): string {
+  		if (this.points.length === 2) {
+  			const curve = new BezierCurve();
+  			curve.setSource(this.getFirstPoint().getPosition());
+  			curve.setTarget(this.getLastPoint().getPosition());
+  			curve.setSourceControl(
+  				this.getFirstPoint()
+  					.getPosition()
+  					.clone()
+  			);
+  			curve.setTargetControl(
+  				this.getLastPoint()
+  					.getPosition()
+  					.clone()
+  			);
+
+  			if (this.sourcePort) {
+  				curve.getSourceControl().translate(...this.calculateControlOffset(this.getSourcePort()));
+  			}
+
+  			if (this.targetPort) {
+  				curve.getTargetControl().translate(...this.calculateControlOffset(this.getTargetPort()));
+  			}
+  			return curve.getSVGCurve();
+  		}
+	}
 }
