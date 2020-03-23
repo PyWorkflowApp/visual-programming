e86370eb2edea2dc88971a99c17eff37f4a0135b
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-03-22 19:46:18 -0500

    Uses parent behavior to define the curve

--- front-end/src/components/CustomLink/CustomLinkModel.js
+++ front-end/src/components/CustomLink/CustomLinkModel.js
@@ -1,9 +1,5 @@
-import { BezierCurve } from '@projectstorm/geometry';
 import { DefaultLinkModel } from '@projectstorm/react-diagrams';
 
-/**
- * Example of a custom model using pure javascript
- */
 export class CustomLinkModel extends DefaultLinkModel {
       constructor() {
         super({
@@ -13,33 +9,11 @@ export class CustomLinkModel extends DefaultLinkModel {
       }
 
     getSVGPath(): string {
-  		if (this.points.length !== 2 || this.isLastPositionDefault()) {
+  		if (this.isLastPositionDefault()) {
         return;
       }
 
-			const curve = new BezierCurve();
-			curve.setSource(this.getFirstPoint().getPosition());
-			curve.setTarget(this.getLastPoint().getPosition());
-			curve.setSourceControl(
-				this.getFirstPoint()
-					.getPosition()
-					.clone()
-			);
-			curve.setTargetControl(
-				this.getLastPoint()
-					.getPosition()
-					.clone()
-			);
-
-			if (this.sourcePort) {
-				curve.getSourceControl().translate(...this.calculateControlOffset(this.getSourcePort()));
-			}
-
-			if (this.targetPort) {
-        curve.getTargetControl().translate(...this.calculateControlOffset(this.getTargetPort()));
-			}
-
-			return curve.getSVGCurve();
+		  return super.getSVGPath();
 	}
 
   isLastPositionDefault() {
