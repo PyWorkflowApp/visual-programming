401c4585dd01ef8363a4fd6b5b3e9b1242a6be30
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-03-22 19:41:38 -0500

    Ignores tracing curve if last point is default

--- front-end/src/components/CustomLink/CustomLinkModel.js
+++ front-end/src/components/CustomLink/CustomLinkModel.js
@@ -13,29 +13,36 @@ export class CustomLinkModel extends DefaultLinkModel {
       }
 
     getSVGPath(): string {
-  		if (this.points.length === 2) {
-  			const curve = new BezierCurve();
-  			curve.setSource(this.getFirstPoint().getPosition());
-  			curve.setTarget(this.getLastPoint().getPosition());
-  			curve.setSourceControl(
-  				this.getFirstPoint()
-  					.getPosition()
-  					.clone()
-  			);
-  			curve.setTargetControl(
-  				this.getLastPoint()
-  					.getPosition()
-  					.clone()
-  			);
+  		if (this.points.length !== 2 || this.isLastPositionDefault()) {
+        return;
+      }
 
-  			if (this.sourcePort) {
-  				curve.getSourceControl().translate(...this.calculateControlOffset(this.getSourcePort()));
-  			}
+			const curve = new BezierCurve();
+			curve.setSource(this.getFirstPoint().getPosition());
+			curve.setTarget(this.getLastPoint().getPosition());
+			curve.setSourceControl(
+				this.getFirstPoint()
+					.getPosition()
+					.clone()
+			);
+			curve.setTargetControl(
+				this.getLastPoint()
+					.getPosition()
+					.clone()
+			);
 
-  			if (this.targetPort) {
-  				curve.getTargetControl().translate(...this.calculateControlOffset(this.getTargetPort()));
-  			}
-  			return curve.getSVGCurve();
-  		}
+			if (this.sourcePort) {
+				curve.getSourceControl().translate(...this.calculateControlOffset(this.getSourcePort()));
+			}
+
+			if (this.targetPort) {
+        curve.getTargetControl().translate(...this.calculateControlOffset(this.getTargetPort()));
+			}
+
+			return curve.getSVGCurve();
 	}
+
+  isLastPositionDefault() {
+    return this.getLastPoint().getX() === 0 && this.getLastPoint().getY() === 0;
+  }
 }
