f701c8c6b814d50e9cf1b3c755969afb25c1d617
Diego Struk <diegostruk#g.harvard.edu>
2020-03-24 09:10:39 -0400

    Fix: removing react framework dependency.

--- vp/vp/settings.py
+++ vp/vp/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     # Project apps
     'workflow.apps.WorkflowConfig',
     'node.apps.NodeConfig',
