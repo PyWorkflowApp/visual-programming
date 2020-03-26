fe8e17c89e857994114394a2aeea511b814bb139
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-03-26 17:20:06 -0500

    Cleans up code

--- front-end/src/components/VPLink/VPLinkFactory.js
+++ front-end/src/components/VPLink/VPLinkFactory.js
@@ -1,6 +1,4 @@
-import * as React from 'react';
 import { VPLinkModel } from './VPLinkModel';
-import { VPLinkWidget } from './VPLinkWidget';
 import { DefaultLinkFactory } from '@projectstorm/react-diagrams';
 
 export class VPLinkFactory extends DefaultLinkFactory {
@@ -8,8 +6,4 @@ export class VPLinkFactory extends DefaultLinkFactory {
     generateModel(): VPLinkModel {
       return new VPLinkModel();
     }
-
-    generateReactWidget(event): JSX.Element {
-  		return <VPLinkWidget link={event.model} diagramEngine={this.engine} />;
-  	}
 }
