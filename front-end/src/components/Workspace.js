11f762a1efba91dd227f30b0eff154abe1e3a352
Samir Reddigari <samirreddigari@gmail.com>
2020-03-28 23:55:22 -0400

    feat(ui): save and load diagrams as JSON

--- front-end/src/components/Workspace.js
+++ front-end/src/components/Workspace.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import * as _ from 'lodash';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import createEngine, { DiagramModel } from '@projectstorm/react-diagrams';
 import { CanvasWidget } from '@projectstorm/react-canvas-core';
 import { VPLinkFactory } from './VPLink/VPLinkFactory';
@@ -21,6 +21,9 @@ class Workspace extends React.Component {
         this.engine.setModel(this.model);
         this.engine.setMaxNumberPointsPerLink(0);
         this.state = {nodes: []};
+        this.save = this.save.bind(this);
+        this.load = this.load.bind(this);
+        this.clear = this.clear.bind(this);
     }
 
     componentDidMount() {
@@ -29,6 +32,39 @@ class Workspace extends React.Component {
             return resp.json();
         }
         getNodes().then(nodes => this.setState({nodes: nodes}));
+    }
+
+    /**
+     * serialize model and download as JSON file
+     */
+    save() {
+        const data = JSON.stringify(this.model.serialize());
+        const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(data)
+        const anchor = document.createElement("a")
+        anchor.href = dataStr;
+        anchor.download = "diagram.json";
+        anchor.click();
+        anchor.remove();
+    }
+
+    /**
+     * Load diagram JSON and render
+     * @param diagramData: serialized diagram JSON
+     */
+    load(diagramData) {
+        this.model.deserializeModel(diagramData, this.engine);
+        // redraw is buggy if you don't wait a little bit
+        setTimeout(() => this.engine.repaintCanvas(), 100);
+    }
+
+    /**
+     * Remove all nodes from diagram
+     */
+    clear() {
+        if (window.confirm("Clear diagram?")) {
+            this.model.getNodes().forEach(n => n.remove());
+            this.engine.repaintCanvas();
+        }
     }
 
     render() {
@@ -43,30 +79,39 @@ class Workspace extends React.Component {
         );
 
         return (
-            <Row className="Workspace">
-                <Col xs={3} className="node-menu">
-                    <div>Drag-and-drop nodes to build a workflow.</div>
-                    <hr />
-                    { menu }
-                </Col>
-                <Col xs={9}>
-                    <div style={{position: 'relative', flexGrow: 1}}
-                        onDrop={event => {
-                            var data = JSON.parse(event.dataTransfer.getData('storm-diagram-node'));
-                            var node = new CustomNodeModel(data);
-                            var point = this.engine.getRelativeMousePoint(event);
-                            node.setPosition(point);
-                            this.model.addNode(node);
-                            this.forceUpdate();
+            <>
+                <Row className="mb-3">
+                    <Col md={12}>
+                        <Button size="sm" onClick={this.save}>Save</Button>{' '}
+                        <FileUpload handleFile={this.load}/>{' '}
+                        <Button size="sm" onClick={this.clear}>Clear</Button>
+                    </Col>
+                </Row>
+                <Row className="Workspace">
+                    <Col xs={3} className="node-menu">
+                        <div>Drag-and-drop nodes to build a workflow.</div>
+                        <hr />
+                        { menu }
+                    </Col>
+                    <Col xs={9}>
+                        <div style={{position: 'relative', flexGrow: 1}}
+                            onDrop={event => {
+                                var data = JSON.parse(event.dataTransfer.getData('storm-diagram-node'));
+                                var node = new CustomNodeModel(data);
+                                var point = this.engine.getRelativeMousePoint(event);
+                                node.setPosition(point);
+                                this.model.addNode(node);
+                                this.forceUpdate();
+                            }}
+                        onDragOver={event => {
+                                event.preventDefault();
                         }}
-                    onDragOver={event => {
-                            event.preventDefault();
-                    }}
-                    >
-                        <CanvasWidget className="diagram-canvas" engine={this.engine} />
-                    </div>
-                </Col>
-            </Row>
+                        >
+                            <CanvasWidget className="diagram-canvas" engine={this.engine} />
+                        </div>
+                    </Col>
+                </Row>
+            </>
         );
     }
 }
@@ -84,6 +129,30 @@ function NodeMenuItem(props) {
             style={{ color: props.color }}>
             {props.name}
         </li>
+    )
+}
+
+
+function FileUpload(props) {
+    const input = useRef(null);
+    const readFile = file => {
+        const fr = new FileReader();
+        fr.addEventListener("load", () => {
+            const data = JSON.parse(fr.result);
+            props.handleFile(data);
+        });
+        fr.readAsText(file);
+    };
+    const onFileSelect = e => {
+        e.preventDefault();
+        readFile(input.current.files[0]);
+    };
+    return (
+        <>
+        <input type="file" ref={input} onChange={onFileSelect}
+            style={{display: "none"}} />
+        <Button size="sm" onClick={() => input.current.click()}>Load</Button>
+        </>
     )
 }
 
