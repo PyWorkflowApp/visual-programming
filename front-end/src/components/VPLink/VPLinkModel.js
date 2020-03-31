066eeb237042eeddb9745e59d24b8147ff63e1ba
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-03-30 19:23:13 -0500

    Adds override to notify when a link is deleted

--- front-end/src/components/VPLink/VPLinkModel.js
+++ front-end/src/components/VPLink/VPLinkModel.js
@@ -20,4 +20,16 @@ export class VPLinkModel extends DefaultLinkModel {
     isLastPositionDefault() {
       return this.getLastPoint().getX() === 0 && this.getLastPoint().getY() === 0;
     }
+
+    /**
+     * TODO: Notify backend the link has been removed
+    */
+    remove() {
+      const sourcePort = this.getSourcePort(); // PortModel
+      const sourceNode = sourcePort.getNode(); // NodeModel
+      const targetPort = this.getTargetPort(); // PortModel
+      const targetNode = targetPort.getNode(); // NodeModel
+      
+      super.remove();
+    }
 }
