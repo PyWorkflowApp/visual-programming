654dc100f32ed76d7d0081df310bedbc9626d01a
Samir Reddigari <samirreddigari@gmail.com>
2020-03-31 19:11:00 -0400

    feat(ui): hit new node endpoint when creating visual node

--- front-end/src/components/Workspace.js
+++ front-end/src/components/Workspace.js
@@ -83,13 +83,33 @@ class Workspace extends React.Component {
         }
     }
 
+    async handleNodeCreation(event) {
+        const data = JSON.parse(event.dataTransfer.getData('storm-diagram-node'));
+        if (!data) return;
+        const node = new CustomNodeModel(data),
+            point = this.engine.getRelativeMousePoint(event);
+        node.setPosition(point);
+        data.node_id = node.options.id;
+        const resp = await fetch("/node/", {
+            method: "POST",
+            body: JSON.stringify(data)
+        });
+        if (resp.status === 200) {
+            this.model.addNode(node);
+            this.forceUpdate();
+            console.log(await resp.json());
+        } else {
+            console.log("Failed to create node on back end.")
+        }
+    }
+
     render() {
         // construct menu from JSON of node types
         const menu = _.map(this.state.nodes, (items, section) =>
             <div key={`node-menu-${section}`}>
                 <b>{section}</b>
                 <ul>
-                { _.map(items, item => <NodeMenuItem {...item} />) }
+                { _.map(items, item => <NodeMenuItem {...item} key={item.node_key} />) }
                 </ul>
             </div>
         );
@@ -111,18 +131,8 @@ class Workspace extends React.Component {
                     </Col>
                     <Col xs={10}>
                         <div style={{position: 'relative', flexGrow: 1}}
-                            onDrop={event => {
-                                var data = JSON.parse(event.dataTransfer.getData('storm-diagram-node'));
-                                var node = new CustomNodeModel(data);
-                                var point = this.engine.getRelativeMousePoint(event);
-                                node.setPosition(point);
-                                this.model.addNode(node);
-                                this.forceUpdate();
-                            }}
-                        onDragOver={event => {
-                                event.preventDefault();
-                        }}
-                        >
+                            onDrop={event => this.handleNodeCreation(event)}
+                            onDragOver={event => event.preventDefault() }>
                             <CanvasWidget className="diagram-canvas" engine={this.engine} />
                         </div>
                     </Col>
