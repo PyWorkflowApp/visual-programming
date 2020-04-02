f9f0809fa56868e077d9f65a88ab15f7205b18dd
Samir Reddigari <samirreddigari@gmail.com>
2020-04-02 13:23:28 -0400

    fix: revert node factory changes

--- pyworkflow/pyworkflow/node_factory.py
+++ pyworkflow/pyworkflow/node_factory.py
@@ -1,39 +1,37 @@
 from .node import *
 
 
-def node_factory(payload):
+def node_factory(node_info):
     # Create a new Node with info
     # TODO: should perform error-checking or add default values if missing
-    node_info = payload["node_info"]
-    node_config = payload["config"]
     node_type = node_info.get('node_type')
     node_key = node_info.get('node_key')
 
     if node_type == 'IONode':
-        new_node = io_node(node_key, node_info, node_config)
+        new_node = io_node(node_key, node_info)
     elif node_type == 'ManipulationNode':
-        new_node = manipulation_node(node_key, node_info, node_config)
+        new_node = manipulation_node(node_key, node_info)
     else:
         new_node = None
 
     return new_node
 
 
-def io_node(node_key, node_info, node_config):
+def io_node(node_key, node_info):
     if node_key == 'ReadCsvNode':
-        return ReadCsvNode(node_info, node_config)
+        return ReadCsvNode(node_info)
     elif node_key == 'WriteCsvNode':
-        return WriteCsvNode(node_info, node_config)
+        return WriteCsvNode(node_info)
     else:
         return None
 
 
-def manipulation_node(node_key, node_info, node_config):
+def manipulation_node(node_key, node_info):
     if node_key == 'JoinNode':
-        return JoinNode(node_info, node_config)
+        return JoinNode(node_info)
     elif node_key == 'PivotNode':
-        return PivotNode(node_info, node_config)
+        return PivotNode(node_info)
     elif node_key == 'multi-in':
-        return ManipulationNode(node_info, node_config)
+        return ManipulationNode(node_info)
     else:
         return None
