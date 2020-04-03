fa1542da7e3ff07b8962cd4116623c79aa4264d9
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-04-02 22:52:29 -0500

    Displays dummy data for tabular view

--- front-end/src/components/CustomNode/GraphView.js
+++ front-end/src/components/CustomNode/GraphView.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import propTypes from 'prop-types';
+import { VariableSizeGrid as Grid } from 'react-window';
 
 function GraphView(props) {
 
     const onClose = () => {
         props.toggleShow();
     };
+
+    const columnWidths = new Array(1000)
+      .fill(true)
+      .map(() => 75 + Math.round(Math.random() * 50));
+
+    const rowHeights = new Array(1000)
+      .fill(true)
+      .map(() => 25 + Math.round(Math.random() * 50));
+
+    const Cell = ({ columnIndex, rowIndex, style }) => (
+      <div style={style}>
+        Item {rowIndex},{columnIndex}
+      </div>
+    );
 
 
     return (
@@ -15,7 +30,16 @@ function GraphView(props) {
                 <Modal.Title><b>{props.node.options.name}</b> View</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-
+            <Grid
+                columnCount={1000}
+                columnWidth={index => columnWidths[index]}
+                height={150}
+                rowCount={1000}
+                rowHeight={index => rowHeights[index]}
+                width={400}
+              >
+                {Cell}
+              </Grid>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={onClose}>Accept</Button>
