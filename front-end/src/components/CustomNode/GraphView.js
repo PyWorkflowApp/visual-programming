7c322e576db2af651364ce29e88884ad1a9df893
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-04-16 01:56:28 -0500

    Adds AutoSize

--- front-end/src/components/CustomNode/GraphView.js
+++ front-end/src/components/CustomNode/GraphView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import propTypes from 'prop-types';
 import { VariableSizeGrid as Grid } from 'react-window';
+import  AutoSizer from 'react-virtualized-auto-sizer';
 import * as API from "../../API";
 
 export default class GraphView extends React.Component {
@@ -9,54 +10,115 @@ export default class GraphView extends React.Component {
     constructor(props) {
       super(props);
       this.key_id = props.node.getNodeId();
-      this.columnCount = 0;
-      this.rowCount = 0;
-      this.state = { data: [] };
+      this.state = {
+        rowCount: 0,
+        columnCount: 0,
+        data: [],
+        keys: [],
+        gridRef: React.createRef()};
     }
 
-    load = () => {
+  onResize = (...args) => {
+      if (this.state.gridRef.current != null) {
+        this.state.gridRef.current.resetAfterIndices({
+          columnIndex: 0,
+          shouldForceUpdate: false
+        });
+      }
+  };
+
+    load = async () => {
           console.log("Loading data");
-          const jsonResponse = API.retrieveData(this.key_id)
-          console.log("response type: " jsonResponse)
-          this.setState({ data: jsonResponse });
+          API.retrieveData(this.key_id)
+          .then(json => {
+            const keys = Object.keys(json);
+            const columnCount = keys.length;
+            console.log("There are " + columnCount + " columns");
+            const rows = Object.keys(json[keys[0]]);
+            const rowCount = rows.length;
+            console.log("There are " + rowCount + " rows")
+            this.setState({ data: json,
+              keys: keys,
+              columnCount: columnCount,
+              rowCount: rowCount });
+          }).catch(err => console.log(err));
     }
 
     onClose = () => {
         this.props.toggleShow();
     };
 
-    columnWidths = new Array(this.columnCount)
+    columnWidths = () => new Array(1000)
       .fill(true)
       .map(() => 75 + Math.round(Math.random() * 50));
 
-    rowHeights = new Array(this.rowCount)
+    rowHeights = () => new Array(765)
       .fill(true)
       .map(() => 25 + Math.round(Math.random() * 50));
 
-    Cell = ({ columnIndex, rowIndex, style }) => (
-      <div style={style}>
-        Item {rowIndex},{columnIndex}
-      </div>
-    );
+    Cell = ({ columnIndex, rowIndex, style }) => {
+      console.log("Showing (" + columnIndex + ", " + rowIndex + ") out of (" + this.state.columnCount + ", " + this.state.rowCount + ")");
+      if (rowIndex === 0) {
+        return (
+          <div style={style}>
+            {this.state.keys[columnIndex]}
+          </div>
+        );
+      }
+
+      return (<div></div>);
+
+      /*return (
+        <div style={style}>
+          {this.state.data[this.keys[columnIndex]][rowIndex]}
+        </div>
+      );*/
+    }
 
 
     render() {
+      if (this.state.columnCount < 1) {
+        return (
+          <Modal show={this.props.show} onHide={this.props.toggleShow} centered>
+          <Modal.Header>
+              <Modal.Title><b>{this.props.node.options.name}</b> View</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          </Modal.Body>
+          <Modal.Footer>
+              <Button variant="secondary" onClick={this.onClose}>Accept</Button>
+              <Button variant="secondary" onClick={this.load}>Load</Button>
+          </Modal.Footer>
+          </Modal>
+        );
+      }
+
+      console.log("Displaying data with " + this.state.columnCount + " columns");
+      console.log("Displaying data with " + this.state.rowCount + " rows");
+
       return (
               <Modal show={this.props.show} onHide={this.props.toggleShow} centered>
               <Modal.Header>
                   <Modal.Title><b>{this.props.node.options.name}</b> View</Modal.Title>
               </Modal.Header>
               <Modal.Body>
+              <div style={{height: '150px', width: '16000px', overflow: 'scroll' }}>
+              <AutoSizer onResize={this.onResize}>
+               {({height, width}) => (
               <Grid
-                  columnCount={this.columnCount}
-                  columnWidth={index => this.columnWidths[index]}
-                  height={150}
-                  rowCount={this.rowCount}
+                  ref={this.state.gridRef}
+                  columnCount={this.state.columnCount}
+                  columnWidth={index => 20}
+                  height={height}
+                  rowCount={this.state.rowCount}
                   rowHeight={index => this.rowHeights[index]}
-                  width={400}
+                  width={width}
                 >
                   {this.Cell}
                 </Grid>
+              )}
+                </AutoSizer>
+                </div>
               </Modal.Body>
               <Modal.Footer>
                   <Button variant="secondary" onClick={this.onClose}>Accept</Button>
