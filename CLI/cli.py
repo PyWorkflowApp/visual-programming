d3e624c4247912a3abbf395d171bba1f88b50e85
Diego Struk <diegostruk#g.harvard.edu>
2020-04-18 17:30:31 -0400

    Catching empty file directory in execute function

--- CLI/cli.py
+++ CLI/cli.py
@@ -14,15 +14,16 @@ pass_config = click.make_pass_decorator(Config, ensure=True)
 @click.option('--file-directory', type=click.Path())
 @pass_config
 def cli(config, file_directory):
-    if file_directory is None:
-        click.echo('Please specify a workflow to run')
-        return
+    print('Unable to run file')
     config.file_directory = file_directory
 
 
 @cli.command()
 @pass_config
 def execute(config):
+    if config.file_directory is None:
+        click.echo('Please specify a workflow to run')
+        return
     try:
         click.echo('Loading workflow file form %s' % config.file_directory)
         Workflow.execute_workflow(config.file_directory)
