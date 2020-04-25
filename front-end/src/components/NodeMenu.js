fe06124506866888654f17710d60017989c36908
Samir Reddigari <samirreddigari@gmail.com>
2020-04-25 18:59:18 -0400

    feat(ui): add tooltips with node docs or errors

--- front-end/src/components/NodeMenu.js
+++ front-end/src/components/NodeMenu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as _ from 'lodash';
-import { Col } from 'react-bootstrap';
+import { Col, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import CustomNodeUpload from "./CustomNodeUpload";
 
 
@@ -34,7 +34,12 @@ export default function NodeMenu(props) {
 
 function NodeMenuItem(props) {
     if (!props.nodeInfo.missing_packages) {
+        const tooltip = props.nodeInfo.doc || "This node has no documentation."
         return (
+            <OverlayTrigger
+                placement="right"
+                delay={{ show: 250, hide: 250 }}
+                overlay={<NodeTooltip message={tooltip} />}>
                 <li className="NodeMenuItem"
                     draggable={true}
                     onDragStart={event => {
@@ -45,11 +50,27 @@ function NodeMenuItem(props) {
                     style={{color: props.nodeInfo.color}}>
                     {props.nodeInfo.name}
                 </li>
+            </OverlayTrigger>
+        )
     } else {
+        let tooltip = "These Python modules could not be imported: ";
+        tooltip += props.nodeInfo.missing_packages.join(", ");
         return (
+            <OverlayTrigger
+                placement="right"
+                delay={{ show: 250, hide: 250 }}
+                overlay={<NodeTooltip message={tooltip} />}>
                 <li className="NodeMenuItem invalid">{props.nodeInfo.filename}</li>
+            </OverlayTrigger>
         )
     }
-    return (
-    )
 }
+
+
+// Overlay with props has to use ref forwarding
+const NodeTooltip = React.forwardRef((props, ref) => {
+    return (
+        <Tooltip {...props} ref={ref}>{props.message}</Tooltip>
+    )
+});
+
