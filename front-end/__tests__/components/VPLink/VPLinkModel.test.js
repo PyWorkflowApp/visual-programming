fb61c11ffe906c130763fca96e446878f2fcaf88
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-04-28 18:41:04 -0500

    Cleans up package-lock.json

--- front-end/__tests__/components/VPLink/VPLinkModel.test.js
+++ front-end/__tests__/components/VPLink/VPLinkModel.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import VPLinkModel from '../../../src/components/VPLink/VPLinkModel';
-import * as API from '../../../src/API';
-import 'babel-polyfill';
+// import * as API from '../../../src/API';
+jest.mock('../../../src/API');
 
 describe('VPLinkModel behaves as expected', () => {
   it('Determine if default is last position', () => {
@@ -9,7 +9,6 @@ describe('VPLinkModel behaves as expected', () => {
       expect(linkModel.isLastPositionDefault()).toBe(true);
   });
   it('remove calls deleteEdge API', () => {
-    jest.mock('../../../src/API');
     const linkModel = new VPLinkModel();
     linkModel.remove();
     expect(API.calls.length).toBe(1);
