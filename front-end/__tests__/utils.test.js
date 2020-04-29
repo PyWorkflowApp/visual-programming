a057d013a4f4a52b8e000062786442a918903560
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-04-28 21:27:08 -0500

    Completes validation of utils.js

--- front-end/__tests__/utils.test.js
+++ front-end/__tests__/utils.test.js
@@ -2,22 +2,33 @@ import React from 'react'
 import { downloadFile } from '../src/utils';
 
 describe('Validates downloadFile', () => {
-  global.URL.createObjectURL = jest.fn();
+  global.URL.createObjectURL = jest.fn(() => 'http://localhost:8080/');
   global.URL.revokeObjectURL = jest.fn();
 
-  const object = {
-    click: jest.fn(),
-  };
 
-  global.document.createElement = jest.fn((tag) => object);
+  it('Validate urls with filename', () => {
+    const object = { click: jest.fn(),};
+    global.document.createElement = jest.fn((tag) => object);
 
-  it('Validate urls', () => {
-    global.URL.createObjectURL = jest.fn(() => 'http://localhost:8080/');
     const createElementMock = jest.fn((type)=>{})
     Object.defineProperty(document, 'createElement', createElementMock);
 
-    downloadFile("{request:data}", "application/json", "diagram.json");
+    downloadFile('{request:data}', 'application/json', 'diagram.json');
     expect(object.click.mock.calls.length).toBe(1);
     expect(object.href).toBe('http://localhost:8080/');
+    expect(object.download).toBe('diagram.json')
+  });
+
+  it('Validate urls with no filename', () => {
+    const object = { click: jest.fn(),};
+    global.document.createElement = jest.fn((tag) => object);
+
+    const createElementMock = jest.fn((type)=>{})
+    Object.defineProperty(document, 'createElement', createElementMock);
+
+    downloadFile('{request:data}', 'application/json', '');
+    expect(object.click.mock.calls.length).toBe(1);
+    expect(object.href).toBe('http://localhost:8080/');
+    expect(object.download).toBe('download')
   });
 });
