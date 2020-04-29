8cd6b43e8385ae114eb2844862c33896639515ca
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-04-28 19:40:24 -0500

    Fixes unit tests

--- front-end/__tests__/components/VPLink/VPLinkModel.test.js
+++ front-end/__tests__/components/VPLink/VPLinkModel.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import VPLinkModel from '../../../src/components/VPLink/VPLinkModel';
-// import * as API from '../../../src/API';
+import * as API from '../../../src/API';
 jest.mock('../../../src/API');
 
 describe('VPLinkModel behaves as expected', () => {
@@ -9,8 +9,9 @@ describe('VPLinkModel behaves as expected', () => {
       expect(linkModel.isLastPositionDefault()).toBe(true);
   });
   it('remove calls deleteEdge API', () => {
+    API.deleteEdge.mockResolvedValue(new Promise((resolve, reject) => {}));
     const linkModel = new VPLinkModel();
     linkModel.remove();
-    expect(API.calls.length).toBe(1);
+    expect(API.deleteEdge.mock.calls.length).toBe(1);
   });
 });
