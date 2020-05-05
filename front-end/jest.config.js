d3271068e2cf852b07570fc693e1d09ef3923005
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-05-04 23:48:24 -0500

    Moves jest's configuration to jest.config.js

---
+++ front-end/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  coverageThreshold: {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
+  },
+  moduleNameMapper: {
+    "\\.(css|less)$": "<rootDir>/__mocks__/css/styleMock.js"
+  },
+  setupFilesAfterEnv: [
+    "./setupTests.js"
+  ]
+};
