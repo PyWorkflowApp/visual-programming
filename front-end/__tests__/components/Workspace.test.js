6290f62bcb869faed3399e69431a9bc46ccd557f
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-05-06 00:55:56 -0500

    Increases code coverage for Workspace

--- front-end/__tests__/components/Workspace.test.js
+++ front-end/__tests__/components/Workspace.test.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import Workspace from '../../src/components/Workspace';
+import FileUpload from '../../src/components/Workspace';
 
 global.console = {log: jest.fn()}
+global.confirm = () => true;
 
 global.fetch = jest.fn(() => Promise.resolve({
   data: [],
@@ -13,5 +15,21 @@ describe('Validates Workspace initialization', () => {
   it('Creates Workspace', () => {
     const workspace = render(<Workspace />);
     expect(workspace).toMatchSnapshot();
+  });
+
+  it('Validates Workspace', () => {
+    const workspace = new Workspace();
+    workspace.componentDidMount();
+    workspace.getAvailableNodes();
+    workspace.getGlobalVars();
+    workspace.clear();
+    workspace.execute();
+
+    expet(globa.fetch.mock.calls.length).toBe(3);
+  });
+
+  it('Display FileUpload', () => {
+    const fileUpload = render(<FileUpload />);
+    expect(fileUpload).toMatchSnapshot();
   });
 });
