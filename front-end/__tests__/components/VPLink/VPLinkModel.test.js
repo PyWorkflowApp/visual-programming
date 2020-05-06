112bf414c393fccc24c8aa7a9e51e09ebc3c0642
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-05-05 21:19:01 -0500

    Increases code coverage for VPLinkModel

--- front-end/__tests__/components/VPLink/VPLinkModel.test.js
+++ front-end/__tests__/components/VPLink/VPLinkModel.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import VPLinkModel from '../../../src/components/VPLink/VPLinkModel';
+import VPPortModel from '../../../src/components/VPPort/VPPortModel'
 import * as API from '../../../src/API';
 jest.mock('../../../src/API');
 
@@ -8,11 +9,33 @@ describe('VPLinkModel behaves as expected', () => {
       const linkModel = new VPLinkModel();
       expect(linkModel.isLastPositionDefault()).toBe(true);
   });
-  
+
   it('remove calls deleteEdge API', () => {
     API.deleteEdge.mockResolvedValue(new Promise((resolve, reject) => {}));
     const linkModel = new VPLinkModel();
     linkModel.remove();
     expect(API.deleteEdge.mock.calls.length).toBe(1);
+  });
+
+  it('Calls targetPortChanged', () => {
+    API.deleteEdge.mockResolvedValue(new Promise((resolve, reject) => {}));
+    const portModel = new VPPortModel({name: 'vp-port-name'});
+    const linkModel = new VPLinkModel();
+    linkModel.setTargetPort(portModel);
+    expect(API.addEdge.mock.calls.length).toBe(1);
+  });
+
+  it('getSVGPath skips execution', () => {
+      const linkModel = new VPLinkModel();
+      linkModel.isLastPositionDefault = jest.fn(() => true);
+      linkModel.getSVGPath();
+      expect(linkModel.isLastPositionDefault.mock.calls.length).toBe(1);
+  });
+
+  it('getSVGPath executes', () => {
+      const linkModel = new VPLinkModel();
+      linkModel.isLastPositionDefault = jest.fn(() => false);
+      linkModel.getSVGPath();
+      expect(linkModel.isLastPositionDefault.mock.calls.length).toBe(1);
   });
 });
