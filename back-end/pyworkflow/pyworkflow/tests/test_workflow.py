2169d9f027d787bc16f05d6846b13abf82d18bb2
Matt Thomas <matthew_thomas@hms.harvard.edu>
2020-05-06 17:26:04 -0400

    test: Add new tests for all classes

--- back-end/pyworkflow/pyworkflow/tests/test_workflow.py
+++ back-end/pyworkflow/pyworkflow/tests/test_workflow.py
@@ -4,11 +4,14 @@ from pyworkflow import Workflow, WorkflowException, Node, NodeException, node_fa
 import networkx as nx
 
 from pyworkflow.tests.sample_test_data import GOOD_NODES, BAD_NODES, DATA_FILES
+
+
 class WorkflowTestCase(unittest.TestCase):
     def setUp(self):
         self.workflow = Workflow("Untitled", root_dir="/tmp", node_dir=os.path.join(os.getcwd(), 'nodes'))
 
         self.read_csv_node = GOOD_NODES["read_csv_node"]
+        self.local_flow_node = GOOD_NODES["string_input"]
         self.write_csv_node = GOOD_NODES["write_csv_node"]
         self.join_node = GOOD_NODES["join_node"]
         self.global_flow_var = GOOD_NODES["global_flow_var"]
@@ -28,6 +31,13 @@ class WorkflowTestCase(unittest.TestCase):
     def test_workflow_name(self):
         self.assertEqual(self.workflow.name, "Untitled")
 
+    def test_workflow_dir_os_error(self):
+        try:
+            # os.makedirs("foobar", 0000)
+            test_workflow = Workflow(node_dir="foobar")
+        except WorkflowException as e:
+            self.assertEqual(e.action, "init workflow")
+
     def test_workflow_node_dir(self):
         self.assertEqual(self.workflow.node_dir, os.path.join(os.getcwd(), 'nodes'))
 
@@ -41,12 +51,38 @@ class WorkflowTestCase(unittest.TestCase):
     def test_workflow_filename(self):
         self.assertEqual(self.workflow.filename, "Untitled.json")
 
+    def test_workflow_from_json(self):
+        new_workflow = Workflow("Untitled", root_dir="/tmp")
+        workflow_copy = Workflow.from_json(self.workflow.to_session_dict())
+
+        self.assertEqual(new_workflow.name, workflow_copy.name)
+
+    def test_workflow_from_json_key_error(self):
+        with self.assertRaises(WorkflowException):
+            new_workflow = Workflow.from_json(dict())
+
+    def test_empty_workflow_to_session(self):
+        new_workflow = Workflow("Untitled", root_dir="/tmp")
+        saved_workflow = new_workflow.to_session_dict()
+
+        workflow_to_compare = {
+            'name': 'Untitled',
+            'root_dir': '/tmp',
+            'graph': Workflow.to_graph_json(new_workflow.graph),
+            'flow_vars': Workflow.to_graph_json(new_workflow.flow_vars),
+        }
+        self.assertDictEqual(new_workflow.to_session_dict(), workflow_to_compare)
+
     ##########################
     # Node lists
     ##########################
     def test_workflow_packaged_nodes(self):
         nodes = self.workflow.get_packaged_nodes()
         self.assertEqual(len(nodes), 5)
+
+    def test_workflow_packaged_nodes_exception(self):
+        result = self.workflow.get_packaged_nodes(root_path="foobar")
+        self.assertIsNone(result)
 
     def test_get_flow_variables(self):
         flow_var_options = self.workflow.get_all_flow_var_options("1")
@@ -59,8 +95,10 @@ class WorkflowTestCase(unittest.TestCase):
         self.assertEqual(successors, ["3", "2"])
 
     def test_fail_get_node_successors(self):
-        with self.assertRaises(WorkflowException):
+        try:
             successors = self.workflow.get_node_successors("100")
+        except WorkflowException as e:
+            self.assertEqual(str(e), "get node successors: The node 100 is not in the digraph.")
 
     def test_fail_get_node_predecessors(self):
         with self.assertRaises(WorkflowException):
@@ -80,6 +118,23 @@ class WorkflowTestCase(unittest.TestCase):
     ##########################
     # Node operations
     ##########################
+    def test_add_custom_node(self):
+        with open(self.workflow.node_path('custom_nodes', 'good_custom_node.py'), 'w') as f:
+            f.write((DATA_FILES['good_custom_node']))
+
+        custom_node_info = {
+            "name": "Custom Node",
+            "node_id": "50",
+            "node_type": "custom_node",
+            "node_key": "MyGoodCustomNode",
+            "is_global": False,
+        }
+
+        node_to_add = Node(custom_node_info)
+        added_node = self.add_node(custom_node_info, "50")
+
+        self.assertDictEqual(node_to_add.__dict__, added_node.__dict__)
+
     def test_add_read_csv_node(self):
         node_to_add = Node(self.read_csv_node)
         added_node = self.add_node(self.read_csv_node, "1")
@@ -132,13 +187,13 @@ class WorkflowTestCase(unittest.TestCase):
     ##########################
     # Edge operations
     ##########################
-    def test_add_xedge_1_to_2(self):
+    def test_add_node_edge_1_to_2(self):
         node_1 = self.workflow.get_node("1")
         node_2 = self.workflow.get_node("2")
         self.add_edge(node_1, node_2)
         return
 
-    def test_add_xedge_duplicated(self):
+    def test_add_node_edge_duplicated(self):
         node_1 = self.workflow.get_node("1")
         node_2 = self.workflow.get_node("2")
 
