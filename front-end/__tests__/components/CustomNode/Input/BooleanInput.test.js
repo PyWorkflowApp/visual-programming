6db529c3fc48485f0b72fa33841cfbc913dc99dc
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-05-09 02:37:22 -0500

    Completes code coverage for BooleanInput

--- front-end/__tests__/components/CustomNode/Input/BooleanInput.test.js
+++ front-end/__tests__/components/CustomNode/Input/BooleanInput.test.js
@@ -7,4 +7,17 @@ describe('Validates BooleanInpupt', () => {
     const booleanInput = shallow(<BooleanInput />);
     expect(booleanInput).toMatchSnapshot();
   });
+
+  it('Checked BooleanInput', () => {
+    const booleanInput = shallow(<BooleanInput
+                        keyName="booleanSelector"
+                        value={false}/>);
+
+    expect(booleanInput.find({name: "booleanSelector"}).prop('checked')).toBe(false);
+
+    booleanInput.find({name: "booleanSelector"}).simulate('change', {target: {checked: true}})
+
+    booleanInput.update();
+    expect(booleanInput.find({name: "booleanSelector"}).prop('checked')).toBe(true);
+  });
 });
