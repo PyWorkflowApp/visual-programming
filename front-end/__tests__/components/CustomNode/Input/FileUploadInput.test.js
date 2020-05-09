480f7c90b998ad7d44eeb38b7a20407fab470b6a
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-05-09 04:17:50 -0500

    Completes code coverage for FlowVariableOverride

--- front-end/__tests__/components/CustomNode/Input/FileUploadInput.test.js
+++ front-end/__tests__/components/CustomNode/Input/FileUploadInput.test.js
@@ -7,4 +7,18 @@ describe('Validates FileUploadInput', () => {
     const input = shallow(<FileUploadInput />);
     expect(input).toMatchSnapshot();
   });
+
+  it('FileUploadInput selects a file', () => {
+    const onChange = jest.fn(() => []);
+    const input = mount(<FileUploadInput
+                        keyName="uploadFile"
+                        onChange={onChange}
+                        />);
+
+    const event = { preventDefault: jest.fn(() => [])};
+    input.find({keyName: "uploadFile"}).simulate('change', event);
+    expect(event.preventDefault.mock.calls.length).toBe(1);
+    input.update();
+
+  });
 });
